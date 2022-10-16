27ed7a9167bfd1250ced5a23848eadbc0dabe0b4
Joy Nyaanga <joynyaanga2024@u.northwestern.edu>
2022-10-16 14:44:36 -0500

    update figures

--- scripts/animation.R
+++ scripts/animation.R
@@ -1,10 +1,11 @@
 library(tidyverse)
 library(cowplot)
 library(gganimate)
+library(gifski)
 
 #load data
 #rawdata <- readr::read_csv("processed/rawdata.csv") 
-pruneddata <- readr::read_csv("processed/pruneddata.csv") %>%
+pruneddata <- readr::read_csv("data/pruneddata.csv") %>%
   dplyr::mutate(TOF = 1.67110915*TOF + 100.31575759,
                 norm.EXT = 15.68506057*norm.EXT -1.04595184,
                 volume = (pi/4)*TOF*(norm.EXT)^2,
@@ -17,12 +18,15 @@ pruneddata <- readr::read_csv("processed/pruneddata.csv") %>%
                                       hour == 7~"07",
                                       hour == 8~"08",
                                       hour == 9~"09",
-                                      hour %in% c(10:42)~as.character(hour)))
-  dplyr::mutate(strain = factor(strain, levels = c("N2", "dpy-1","dpy-5", "lon-3")))
+                                      hour %in% c(10:42)~as.character(hour))) %>%
+  dplyr::mutate(strain = factor(strain, levels = c("N2", "dpy-1","dpy-5", "lon-3"))) %>%
+  dplyr::mutate(Genotype = factor(strain, levels = c("N2", "dpy-1","dpy-5", "lon-3"), labels = c("WT", "dpy-1","dpy-5", "lon-3")))
 
 
+
+#mean tp -- no facet
 anim <- pruneddata %>%
-  dplyr::group_by(strain, hour, timepoint, row, col) %>%
+  dplyr::group_by(strain, Genotype, hour, timepoint) %>%
   dplyr::summarize(median.TOF = median(TOF),
                    median.norm.EXT= median(norm.EXT), 
                    median.norm.red = median(norm.red), .groups = "drop") %>%
@@ -39,19 +43,55 @@ anim <- pruneddata %>%
                 `Molting?` = dplyr::case_when(median.norm.red <= 0.06 & timepoint > 12 ~ "yes", TRUE ~ "no")) %>%
   dplyr::filter(hour <= 55) %>%
   ggplot(.) +
-  aes(x = median.TOF, y = median.norm.EXT) +
-  geom_point(size = 0.6, alpha = 0.5) + 
-  scale_color_manual(values = c("black", "red")) +
-  guides(color = "none") +
-  theme_cowplot(font_size = 18, rel_small = 14/18) + 
-  facet_wrap(~strain, scales = "free") + panel_border() +
-  transition_time(hour) + shadow_mark(future = F, alpha = alpha/4, size = size/4) +
+  aes(x = median.TOF, y = median.norm.EXT, color = Genotype) +
+  geom_point(size = 2, alpha = 1) + 
+  scale_color_manual(values =  c("#e69f00","#009e73","#0072b2","black"), labels = c("WT", expression(italic("dpy-1")),
+                                                                                     expression(italic("dpy-5")),
+                                                                                     expression(italic("lon-3")))) +
+  theme(legend.position="top", legend.background = element_rect(fill="#FFFFFF")) +
+  #guides(color = "none") +
+  theme_cowplot(font_size = 20, rel_small = 16/20) + 
+  #facet_wrap(~strain, scales = "free") + panel_border() +
+  transition_time(hour) + shadow_mark(future = F, alpha = alpha/2, size = size/2) +
   #transition_manual(hour, cumulative = TRUE) +
-  labs(x = expression(paste("Median Animal Length (", mu,"m)")), y = expression(paste("Median Animal Width (", mu,"m)")), 
-       title = "Hour: {frame} of {nframes}") 
+  labs(x = expression(paste("Median Animal Length")), y = expression(paste("Median Animal Width")))
 
+animate(anim, height = 1920, width = 2400, units = "px", res = 300)
 
-animate(anim, duration = 21, nframes = 42, height = 1920, width = 1920, units = "px", res = 300)
+anim_save("figures/sumGIF.gif")
 
-anim_save("animations/quickGIF.gif")
+#facet anim
+anim <- pruneddata %>%
+  dplyr::group_by(strain, Genotype, hour, timepoint, row, col) %>%
+  dplyr::summarize(median.TOF = median(TOF),
+                   median.norm.EXT= median(norm.EXT), 
+                   median.norm.red = median(norm.red), .groups = "drop") %>%
+  dplyr::mutate(stage = case_when(strain %in% c("N2", "dpy-1", "dpy-5", "lon-3") & hour %in% c(1:10) ~ "L1",
+                                  strain == "N2" & hour %in% c(16:22) ~ "L2",
+                                  strain == "N2" & hour %in% c(26:32) ~ "L3",
+                                  strain == "lon-3" & hour %in% c(16:22) ~ "L2",
+                                  strain == "lon-3" & hour %in% c(26:30) ~ "L3",
+                                  strain == "dpy-1" & hour %in% c(19:26) ~ "L2",
+                                  strain == "dpy-1" & hour %in% c(31:35) ~ "L3",
+                                  strain == "dpy-5" & hour %in% c(19:24) ~ "L2",
+                                  strain == "dpy-5" & hour %in% c(29:34) ~ "L3"))  %>%
+  dplyr::mutate(hour = as.numeric(timepoint),
+                `Molting?` = dplyr::case_when(median.norm.red <= 0.06 & timepoint > 12 ~ "yes", TRUE ~ "no")) %>%
+  dplyr::filter(hour <= 55) %>%
+  dplyr::mutate(Genotype = factor(strain, levels = c("N2", "dpy-1","dpy-5", "lon-3"), labels = c("WT", expression(italic("dpy-1")),
+                                                                                                 expression(italic("dpy-5")),
+                                                                                                 expression(italic("lon-3"))))) %>%
+  ggplot(.) +
+  aes(x = median.TOF, y = median.norm.EXT, color = Genotype) +
+  geom_point(size = 1, alpha = 1) +
+  scale_color_manual(values = c("#e69f00","#009e73","#0072b2","black")) +
+  theme(legend.position="top", legend.background = element_rect(fill="#FFFFFF")) +
+  guides(color = "none") +
+  theme_cowplot(font_size = 20, rel_small = 16/20) + 
+  facet_wrap(~Genotype, scales = "free", labeller = label_parsed) + panel_border() +
+  transition_time(hour) + shadow_mark(future = F, alpha = alpha/4, size = size/4) +
+  labs(x = expression(paste("Median Animal Length")), y = expression(paste("Median Animal Width")))
 
+animate(anim, height = 1920, width = 2400, units = "px", res = 300)
+
+anim_save("figures/facetGIF.gif")
--- scripts/plots.R
+++ scripts/plots.R
@@ -3,7 +3,7 @@ library(cowplot)
 
 #load data
 #rawdata <- readr::read_csv("processed/rawdata.csv") 
-pruneddata <- readr::read_csv("processed/pruneddata.csv") %>%
+pruneddata <- readr::read_csv("data/pruneddata.csv") %>%
   dplyr::mutate(TOF = 1.67110915*TOF + 100.31575759,
                 norm.EXT = 15.68506057*norm.EXT -1.04595184,
                 volume = (pi/4)*TOF*(norm.EXT)^2,
@@ -26,7 +26,7 @@ pruneddata <- readr::read_csv("processed/pruneddata.csv") %>%
                                           strain == "dpy-1" & hour %in% c(31:35) ~ "L3",
                                           strain == "dpy-5" & hour %in% c(19:24) ~ "L2",
                                           strain == "dpy-5" & hour %in% c(30:34) ~ "L3")) %>%
-  dplyr::mutate(strain = factor(strain, levels = c("N2", "dpy-1","dpy-5", "lon-3")))
+  dplyr::mutate(Genotype = factor(strain, levels = c("N2", "dpy-1","dpy-5", "lon-3"), labels = c("WT", "dpy-1","dpy-5", "lon-3")))
 
 
 
@@ -42,8 +42,8 @@ pruneddata %>%
   labs(x="Median Length", y = "Median Width", title = "Population Data") + guides(color = "none")
 
 # medians
-pruneddata %>%
-  dplyr::group_by(strain, hour, timepoint, row, col) %>%
+fig1e <- pruneddata %>%
+  dplyr::group_by(strain, hour, timepoint, row, col, Genotype) %>%
   dplyr::summarize(median.Length = median(TOF),
                    median.Width = median(norm.EXT), .groups = "drop") %>%
   dplyr::mutate(stage = case_when(strain %in% c("N2", "dpy-1", "dpy-5", "lon-3") & hour %in% c(1:10) ~ "L1",
@@ -56,123 +56,97 @@ pruneddata %>%
                                   strain == "dpy-5" & hour %in% c(19:24) ~ "L2",
                                   strain == "dpy-5" & hour %in% c(30:34) ~ "L3")) %>%
   dplyr::mutate(strain = factor(strain, levels = c("N2", "dpy-1","dpy-5", "lon-3"))) %>%
+  dplyr::mutate(Genotype = factor(strain, levels = c("N2", "dpy-1","dpy-5", "lon-3"), labels = c("WT", expression(italic("dpy-1")),
+                                                                                                 expression(italic("dpy-5")),
+                                                                                                 expression(italic("lon-3"))))) %>%
   ggplot(.) +
   aes(x = median.Length, y = median.Width) +
   geom_point(size = 0.3) +
-  theme_cowplot(18) +
+  theme_cowplot(font_size = 32, rel_small = 28/32) +
   scale_color_manual(values = c('#4477AA', '#117733', '#DDCC77',"grey")) +
-  facet_wrap(~strain, scales = "free") + panel_border() +
+  facet_wrap(~Genotype, scales = "free", labeller = label_parsed) + panel_border() +
   labs(x="Median Length", y = "Median Width") + guides(color= "none")
+
+fig1e
 
 
 ## plots split out to compare to N2
 plot.red <- function(strains, colorfill) {
   pruneddata %>%
     dplyr::filter(strain %in% strains) %>%
-    dplyr::group_by(strain, hour, timepoint, row, col) %>%
+    dplyr::group_by(strain, hour, timepoint, row, col, Genotype) %>%
     dplyr::summarize(median.red = median(red/volume), .groups = "drop") %>%
     ggplot(.) +
-    aes(x = hour, y = median.red, fill = strain, color = strain) + 
+    aes(x = hour, y = median.red, fill = Genotype, color = Genotype) + 
     geom_boxplot(aes(x = timepoint),outlier.alpha = 0, alpha = 0.7) +
     geom_jitter(size=0.1, width=0.2, alpha=0.2) +
-    geom_smooth(span = 0.18, se = F, size = 0.8, method = "loess", aes(color = strain)) +
-    theme_cowplot(font_size = 22, rel_small = 16/22) +
+    geom_smooth(span = 0.18, se = F, size = 0.8, method = "loess", aes(color = Genotype)) +
+    theme_cowplot(font_size = 32, rel_small = 30/32) +
     labs(x="Time (hours)", y = "Median Fluorescence") +
-    scale_fill_manual(values = colorfill) +
-    scale_color_manual(values = colorfill) +
+    scale_fill_manual(values = colorfill, labels = c("WT", expression(italic("dpy-1")),
+                                                     expression(italic("dpy-5")),
+                                                     expression(italic("lon-3")))) +
+    scale_color_manual(values = colorfill, labels = c("WT", expression(italic("dpy-1")),
+                                                      expression(italic("dpy-5")),
+                                                      expression(italic("lon-3")))) +
     theme(legend.position="top", legend.background = element_rect(fill="#FFFFFF")) +
-    scale_x_discrete(breaks = c("05", 10, 15, 20, 25, 30, 35, 40, 45, 50))
+    scale_x_discrete(breaks = c("05", 10, 15, 20, 25, 30, 35, 40, 45, 50)) 
   
 }
 
 plot.length <- function(strains, colorfill) {
   pruneddata %>%
     dplyr::filter(strain %in% strains) %>%
-    dplyr::group_by(strain, hour, timepoint, row, col) %>%
+    dplyr::group_by(strain, hour, timepoint, row, col, Genotype) %>%
     dplyr::summarize(median.Length = median(TOF), .groups = "drop") %>%
     ggplot(.) +
-    aes(x = hour, y = median.Length, fill = strain, color = strain) + 
+    aes(x = hour, y = median.Length, fill = Genotype, color = Genotype) + 
     geom_boxplot(aes(x = timepoint),outlier.alpha = 0, alpha = 0.7) +
     geom_jitter(size=0.1, width=0.2, alpha=0.2) +
-    theme_cowplot(font_size = 22, rel_small = 16/22) +
+    theme_cowplot(font_size = 32, rel_small = 30/32) +
     labs(x="Time (hours)", y = "Median Length") +
-    scale_fill_manual(values = colorfill) +
-    scale_color_manual(values = colorfill) +
+    scale_fill_manual(values = colorfill, labels = c("WT", expression(italic("dpy-1")),
+                                                     expression(italic("dpy-5")),
+                                                     expression(italic("lon-3")))) +
+    scale_color_manual(values = colorfill, labels = c("WT", expression(italic("dpy-1")),
+                                                      expression(italic("dpy-5")),
+                                                      expression(italic("lon-3")))) +
     theme(legend.position="top", legend.background = element_rect(fill="#FFFFFF")) +
-    scale_x_discrete(breaks = c("05", 10, 15, 20, 25, 30, 35, 40, 45, 50))
+    scale_x_discrete(breaks = c("05", 10, 15, 20, 25, 30, 35, 40, 45, 50)) 
+    
 }
 
 plot.width <- function(strains, colorfill) {
   pruneddata %>%
     dplyr::filter(strain %in% strains) %>%
-    dplyr::group_by(strain, hour, timepoint, row, col) %>%
+    dplyr::group_by(strain, hour, timepoint, row, col, Genotype) %>%
     dplyr::summarize(median.Width = median(norm.EXT), .groups = "drop") %>%
     ggplot(.) +
-    aes(x = hour, y = median.Width, fill = strain, color = strain) + 
+    aes(x = hour, y = median.Width, fill = Genotype, color = Genotype) + 
     geom_boxplot(aes(x = timepoint),outlier.alpha = 0, alpha = 0.7) +
     geom_jitter(size=0.1, width=0.2, alpha=0.2) +
-    theme_cowplot(font_size = 22, rel_small = 16/22) +
+    theme_cowplot(font_size = 32, rel_small = 30/32) +
     labs(x="Time (hours)", y = "Median Width") +
-    scale_fill_manual(values = colorfill) +
-    scale_color_manual(values = colorfill) +
+    scale_fill_manual(values = colorfill, labels = c("WT", expression(italic("dpy-1")),
+                                                     expression(italic("dpy-5")),
+                                                     expression(italic("lon-3")))) +
+    scale_color_manual(values = colorfill, labels = c("WT", expression(italic("dpy-1")),
+                                                      expression(italic("dpy-5")),
+                                                      expression(italic("lon-3")))) +
     theme(legend.position="top", legend.background = element_rect(fill="#FFFFFF")) +
-    scale_x_discrete(breaks = c("05", 10, 15, 20, 25, 30, 35, 40, 45, 50))
+    scale_x_discrete(breaks = c("05", 10, 15, 20, 25, 30, 35, 40, 45, 50)) 
 }
 
-plot.volume <- function(strains, colorfill) {
-  pruneddata %>%
-    dplyr::filter(strain %in% strains) %>%
-    dplyr::group_by(strain, hour, timepoint, row, col) %>%
-    dplyr::summarize(median.Width = median(volume), .groups = "drop") %>%
-    ggplot(.) +
-    aes(x = hour, y = median.Width, fill = strain, color = strain) + 
-    geom_boxplot(aes(x = timepoint),outlier.alpha = 0, alpha = 0.7) +
-    geom_jitter(size=0.1, width=0.2, alpha=0.2) +
-    theme_cowplot(font_size = 22, rel_small = 16/22) +
-    labs(x="Time (hours)", y = "Median Volume") +
-    scale_fill_manual(values = colorfill) +
-    scale_color_manual(values = colorfill) +
-    theme(legend.position="top", legend.background = element_rect(fill="#FFFFFF"))  +
-    scale_x_discrete(breaks = c("05", 10, 15, 20, 25, 30, 35, 40, 45, 50)) +
-    scale_y_log10()
-}
 
-#1200x700
-a <- plot.volume(c("N2", "lon-3", "dpy-1", "dpy-5"), c("#e69f00","#009e73","#0072b2","black"))
-b <- plot.volume(c("N2", "lon-3"), c("#0072b2","black"))
-c <- plot.volume(c("N2", "dpy-1"), c("#e69f00","black"))
-d <- plot.volume(c("N2", "dpy-5"), c("#009e73","black"))
-
-cowplot::plot_grid(a, b, c, d, ncol = 2, nrow = 2, align = "hv")
-
-
-
+#1300x700
 
 a <- plot.red(c("N2", "lon-3", "dpy-1", "dpy-5"), c("#e69f00","#009e73","#0072b2","black"))
 b <- plot.length(c("N2", "lon-3", "dpy-1", "dpy-5"), c("#e69f00","#009e73","#0072b2","black"))
 c <- plot.width(c("N2", "lon-3", "dpy-1", "dpy-5"), c("#e69f00","#009e73","#0072b2","black"))
 
-cowplot::plot_grid(a, b, c, ncol = 2, nrow = 2, align = "hv")
+fig1r <- cowplot::plot_grid(a, b, c, ncol = 1, nrow = 3, align = "hv")
 
 
-
-clean %>% 
-  dplyr::mutate(TOF = 1.67110915*TOF + 100.31575759,
-                norm.EXT = 15.68506057*norm.EXT -1.04595184) %>%
-  #dplyr::filter(strain == "dpy-5") %>%
-  dplyr::group_by(hour, strain, plate, row, col) %>%
-  #dplyr::group_by(hour, strain) %>%
-  dplyr::summarize(median.TOF = median(TOF), median.norm.EXT = median(norm.EXT)) %>%
-  ungroup() %>%
-  ggplot() + 
-  #aes(x = TOF, y = norm.EXT) +
-  aes(x = median.TOF, y = median.norm.EXT) + 
-  geom_point(aes(color = hour), size = 0.5) +
-  #geom_point(size = 0.01, color="grey", alpha=0.5) + 
-  theme_bw() + labs(x="Median Length", y="Median Width") + 
-  facet_wrap(~strain, scales = "free") 
-  geom_smooth(se = F, span = 0.05)
-  geom_path(aes(color = hour))
-
+ggsave("figures/Fig1r.png",fig1r, bg = "white", width = 10, height = 24)
 
 
